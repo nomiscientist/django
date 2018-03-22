7d2200ade9619b8dd4d840035c056a288422fead
Muhammad Faisal <mfaisalpk@outlook.com>
2018-03-22 13:35:09 +0500

    testproject and accounts app added

---
+++ testproject/accounts/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+# there are two type of views class based and function based
+
+def home(request):
+    # return  HttpResponse("Hello Pakistan from Home page of accounts!!")
+
+    numbers = [1,2,3,4,5]
+
+    name = "mfaisal"
+    args = {"name":name,"numbers":numbers}
+
+    return render(request, 'accounts/login.html',args)
+
+
