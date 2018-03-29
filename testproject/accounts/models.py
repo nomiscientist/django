f19e3d722db58b9a51f549282e40c567eff33b44
Muhammad Faisal <mfaisalpk@outlook.com>
2018-03-29 13:55:28 +0500

    Page Logout, Navigation bar added

--- testproject/accounts/models.py
+++ testproject/accounts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models import signals
 
 # Create your models here.
 
@@ -9,3 +10,10 @@ class UserProfile(models.Model):
     website = models.URLField(default='')
     city = models.CharField(max_length=50,default='')
     phone = models.IntegerField(default=0)
+
+def createProfile(sender, **kwargs):
+    if kwargs["created"]:
+        user_profile = UserProfile.objects.create(user=kwargs['instance'])
+
+
+signals.post_save.connect(createProfile,sender=User)
--- testproject/testproject/urls.py
+++ testproject/testproject/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
+from testproject import views
 
 urlpatterns = [
+    url(r'^$',views.loginRedirect, name="loginRedirect"),
     url(r'^admin/', admin.site.urls),
     url(r'^account/',include('accounts.urls'))
 ]
