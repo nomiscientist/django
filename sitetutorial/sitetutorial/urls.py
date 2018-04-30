c2f409a7a380a801d4828cb29a40d648652b8ed5
Muhammad Faisal <mfaisalpk@outlook.com>
2018-04-30 13:19:24 +0500

    Your Second View Django - Dynamic Content

--- sitetutorial/sitetutorial/urls.py
+++ sitetutorial/sitetutorial/urls.py
@@ -6,5 +6,7 @@ from sitetutorial import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^hello/$',views.hello)
+    url(r'^hello/$',views.hello),
+    url(r'^time/$',views.current_datetime),
+    url(r'^time/plus/(\d{1,2})/$',views.future_datetime),
 ]
--- sitetutorial/sitetutorial/views.py
+++ sitetutorial/sitetutorial/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+
+import datetime
 
 def hello(request):
     return HttpResponse("Hello Pakistan!")
+
+def current_datetime(request):
+    return HttpResponse("It is now: %s" % datetime.datetime.now())
+
+def future_datetime(request, fhours):
+    try:
+        fhours = int(fhours)
+    except ValueError:
+        raise Http404()
+
+    ftime = datetime.datetime.now() + datetime.timedelta(hours=fhours)
+    return HttpResponse("Future Hour(s): %s Gets us: %s"%(fhours,ftime))
+    
