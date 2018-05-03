cb033959483294ce2d458168477495b9e77dd0d2
Muhammad Faisal <mfaisalpk@outlook.com>
2018-05-04 00:51:13 +0500

    Django Form Validation

--- sitetutorial/books/views.py
+++ sitetutorial/books/views.py
@@ -9,19 +9,20 @@ from books import models
 
 
 def search(request):
-    error = False
+    errors = []
     if 'q' in request.GET:
         q = request.GET['q']
         if not q:
-            print(q)
-            error = True
+            errors.append("Please submit search term.")
+        elif len(q) >= 20:
+            errors.append("Please submit a search term 20 characters or shorter.")
         else:
             book_obj = models.Book.objects.filter(title__icontains=q)
             args = {'books':book_obj,'query':q}
             return render(request,"books/search_results.html",args)
             # q = "Please submit a search term."
             # return HttpResponse(q)
-    return render(request,"books/search_form.html",{"error":error})
+    return render(request,"books/search_form.html",{"errors":errors})
 
 
 
